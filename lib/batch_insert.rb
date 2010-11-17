9557f4bbf4d4a32e882d323fa1dcd3055d93158b
Shaun Mangelsdorf <s.mangelsdorf@gmail.com>
2010-11-18 08:18:37 +1000

    Added batch_size support

--- lib/batch_insert.rb
+++ lib/batch_insert.rb
@@ -3,24 +3,21 @@ module ActiveRecord
   module BatchInsert
     def self.included(base)
       base.class_inheritable_accessor :batched_inserts
+      base.class_inheritable_accessor :batched_insert_opts
       base.extend ClassMethods
     end
 
     module ClassMethods
-      def batch_insert
+      def batch_insert(opts={})
         self.batched_inserts = returning(batched_inserts) do
           self.batched_inserts = []
-          yield
 
-          unless self.batched_inserts.empty?
-            column_names = columns.map(&:name).sort - [primary_key]
-            connection.execute %Q{
-              INSERT INTO #{connection.quote_table_name(table_name)}
-              (#{column_names.map{|n| connection.quote_column_name(n)}.join(',')})
-              VALUES
-              #{batch_insert_values_string(column_names)}
-            }.gsub(/\s+/,' ').squeeze(' ').strip
+          self.batched_insert_opts = returning(batched_insert_opts) do
+            self.batched_insert_opts = opts
+            yield
           end
+
+          flush_batch_insert_queue
         end
       end
 
@@ -30,10 +27,27 @@ module ActiveRecord
         end.join ','
       end
 
+      def flush_batch_insert_queue
+        unless self.batched_inserts.empty?
+          column_names = columns.map(&:name).sort - [primary_key]
+          connection.execute %Q{
+            INSERT INTO #{connection.quote_table_name(table_name)}
+            (#{column_names.map{|n| connection.quote_column_name(n)}.join(',')})
+            VALUES
+            #{batch_insert_values_string(column_names)}
+          }.gsub(/\s+/,' ').squeeze(' ').strip
+
+          batched_inserts.clear
+        end
+      end
+
       def insert(opts={})
         returning new(opts) do |obj|
           raise RecordInvalid.new(obj) unless obj.valid?
           self.batched_inserts << obj.attributes
+
+          limit = batched_insert_opts[:batch_size].to_i
+          flush_batch_insert_queue if limit > 0 and self.batched_inserts.length >= limit
         end
       end
     end
