92c004a5146d14ff2045ed9382f537683bb29a8c
Rich Wallett <richwallett@gmail.com>
2017-09-26 13:18:07 -0700

    pass a single argument to quote_value

--- lib/batch_insert.rb
+++ lib/batch_insert.rb
@@ -17,7 +17,7 @@ module BatchInsert
 
       def batch_insert_values_string(column_names)
         self.batched_inserts.collect do |attributes|
-          "(#{column_names.map{|n| quote_value(attributes.fetch(n){attributes[n.to_sym]}, columns_hash[n])}.join(',')})"
+          "(#{column_names.map{|n| quote_value(attributes.fetch(n){attributes[n.to_sym]})}.join(',')})"
         end.join ','
       end
 
